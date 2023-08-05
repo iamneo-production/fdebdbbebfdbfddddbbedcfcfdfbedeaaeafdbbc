ecd7e9c8102304fb7c0c80dd9b32b5a849b752e6
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:20:51 +0000

    Backup Commit

--- quiz app/quiz-app-master/src/components/UI/Banner/Banner.js
+++
@@ -1,5 +0,0 @@
-const Banner = (props) => {
-    return <div>You have answered {props.correct} / 5  Correctly</div>;
-};
-
-export default Banner;
--- quiz app/quiz-app-master/src/components/UI/Button/Button.js
+++
@@ -1,5 +0,0 @@
-const Button = (props) => {
-    return <button onClick={props.handler} disabled={props.disabledState} value={props.value}>{props.children}</button>;
-};
-
-export default Button;
