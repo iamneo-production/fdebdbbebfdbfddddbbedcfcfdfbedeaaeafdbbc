498e64311595e55eca77716dfd873d6bc6042100
Iamneo <techadmin@iamneo.ai>
2023-08-05 11:19:32 +0000

    Backup Commit

--- reactapp/src/tests/App.test.js
+++ reactapp/src/tests/App.test.js
@@ -28,7 +28,7 @@ describe('Testing App Component', () => {
     });
 
     test('testcase4', () => {
-        const wrapper = mount(<App />);
+        wrapper = mount(<App />);
         wrapper.find('button').simulate('click')
         wrapper.find('button').forEach(node => {
             expect(node.simulate('click'));
